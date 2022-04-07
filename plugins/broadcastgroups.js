9e22be0d72117ea7e3d52c8c6862a3f712de58d4
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-04-07 23:59:14 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
     conn.reply(m.chat, `_Send a broadcast message to ${groups.length} group_\nestimation complete ${groups.length * 1.5} seconds`, m)
     for (let id of groups) {
         await delay(1500)
-        await conn.send2ButtonLoc(id, logo,'*—「 Zifabotz Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Sewa', '.sewa')
+        await conn.send2ButtonLoc(id, logo,'*—「 OSCARR BOTZ Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Sewa', '.sewa')
     }
     m.reply('_*Broadcast berhasil di kirim*_')
 }
