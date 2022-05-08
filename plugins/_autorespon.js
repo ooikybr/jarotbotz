f7502047df9b58a845b98c6a6fcba5cf9995ea3d
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-09 02:20:49 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -32,9 +32,9 @@ handler.all = async function (m, { isBlocked }) {
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
         this.send2ButtonLoc(m.chat, logo, `
 ╭━━〔 List Sewa〕━✧
-│⬦ *1 Bulan* :      *Rp 15.000*
-│⬦ *4 bulan* :      *Rp 30.000*
-│⬦ *8 bulan* :      *Rp 40.000*
+│⬦ *1 Bulan* :      *Rp 25.000*
+│⬦ *4 bulan* :      *Rp 50.000*
+│⬦ *8 bulan* :      *Rp 80.000*
 ┝━━〔Payment〕━✧
 │⬦ Dana
 │⬦ Gopay
@@ -69,7 +69,7 @@ Note: Syarat dan Ketentuan mungkin berlaku`.trim(), wm, 'Menu', '#menu', 'Owner'
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Im Oscarbotz 🤖 || ⏰ Aktif selama ${uptime} jam |(*delay*🌕)| 👥 User : ${Object.keys(global.db.data.users).length} User || 🎐 Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik by.rozi'}`).catch(_ => _)
+        await this.setStatus(`Im BOT JR 🤖 || ⏰ Aktif selama ${uptime} jam |(*delay*🌕)| 👥 User : ${Object.keys(global.db.data.users).length} User || 🎐 Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik by.JAROT'}`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
