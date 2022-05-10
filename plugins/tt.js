b7ba1f36bcb9b6698a4d06c623505c517b41629f
Jarotr <101513858+Jarotr@users.noreply.github.com>
2022-05-10 13:31:02 +0700

    Update tt.js

--- plugins/tt.js
+++ plugins/tt.js
@@ -2,7 +2,21 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-*UDAH VAKUM MAIN TIKTOK!*
+╭═══════════════════════
+
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪TIKTOK⳹ ❋ཻུ۪۪ 〕 ❉────── 
+
+║│➸ *_TIKTOK OWNER_*
+
+║│➸  *https://vt.tiktok.com/ZSduPhdNG/*
+
+╰─────────❉
+
+▌│█║▌║▌║║▌║▌║█│▌
+
+⳹ ❋ཻུ۪۪BOT JR⳹ ❋ཻུ۪۪⸙ by.JAROT
+
+`
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
 }
