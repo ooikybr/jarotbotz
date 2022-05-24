19d4a24e5779da6d0815e4d578520092b7167d87
ooikybr <105967743+ooikybr@users.noreply.github.com>
2022-05-24 18:27:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,13 +41,13 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'JAROTT'
+global.packname = 'VALDES'
 global.author = 'JAROTT'
 
 global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
 global.eror = '「❗」Server LAGI ERROR COK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© OSCAR BOTZ' //change the watermark 
+global.watermark = '© VALDES BOTZ' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/b224cf1834146e292aa8d.jpg'
 
